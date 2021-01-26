fcd399d893fe1eaf74791be294e8307efb7f263d
akared-coke <aka.red.coke@gmail.com>
2021-01-26 12:40:32 +0800

    Destroy enemy if it reaches destination.

--- Assets/MissileCommand/Scripts/Enemies/TargetingEnemy.cs
+++ Assets/MissileCommand/Scripts/Enemies/TargetingEnemy.cs
@@ -11,5 +11,12 @@ public class TargetingEnemy : Enemy
     public override void Move()
     {
         transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        if (transform.position == target)
+        {
+            if (gameObject != null)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
