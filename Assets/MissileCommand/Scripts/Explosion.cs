17588462c4a1aefd329be1a312ea2a0370831c5b
akared-coke <aka.red.coke@gmail.com>
2021-01-26 11:57:35 +0800

    Add explosion.

---
+++ Assets/MissileCommand/Scripts/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class Explosion : MonoBehaviour
+{
+    public float power;
+    public Vector3 initialScale;
+
+    public void Start()
+    {
+        transform.localScale = initialScale;
+        Expand();
+    }
+
+    private void Expand()
+    {
+        transform.DOScale(initialScale * power, 2f).OnComplete(() =>
+        {
+            Destroy(gameObject);
+        });
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/MissileCommand/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void OnPlayButtonTapped()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnBackToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
