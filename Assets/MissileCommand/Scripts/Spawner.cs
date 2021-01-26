f0ec9a4e81e0a0263abe75c6493deabe0bc4158b
akared-coke <aka.red.coke@gmail.com>
2021-01-26 11:08:59 +0800

    Initial spawner.

--- Assets/MissileCommand/Scripts/Spawner.cs
+++ Assets/MissileCommand/Scripts/Spawner.cs
@@ -1,19 +1,64 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Enemy enemyToSpawn;
 
-    // Start is called before the first frame update
-    void Start()
+    public float initialSpawnRate;
+    public float spawnRateDecay;
+    public float minSpawnRate;
+    public float spawnDelay;
+
+    [SerializeField]
+    private RectTransform spawnArea;
+
+    private float _currentSpawnRate;
+    private float _lastSpawn;
+    private bool _canSpawn;
+
+    private void Start()
     {
-        
+        StartSpawning();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartSpawning()
     {
-        
+        _currentSpawnRate = initialSpawnRate;
+        _lastSpawn = 0;
+        StartCoroutine(StartSpawningCoroutine());
+    }
+
+    private IEnumerator StartSpawningCoroutine()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        _canSpawn = true;
+    }
+
+    private void Update()
+    {
+        if(!_canSpawn)
+        {
+            return;
+        }
+
+        if(Time.time - _lastSpawn >= _currentSpawnRate)
+        {
+            Spawn();
+            _currentSpawnRate = Mathf.Max(minSpawnRate, _currentSpawnRate - spawnRateDecay);
+            _lastSpawn = Time.time;
+        }
+    }
+
+    private void Spawn()
+    {
+        Enemy enemy = Instantiate(enemyToSpawn, GetSpawnPoint(), Quaternion.identity);
+    }
+
+    private Vector2 GetSpawnPoint()
+    {
+        float randomX = Random.Range(spawnArea.position.x - spawnArea.rect.width / 2, spawnArea.position.x + spawnArea.rect.width / 2);
+        Vector2 spawnpoint = new Vector2(randomX, spawnArea.position.y);
+        return spawnpoint;
     }
 }
