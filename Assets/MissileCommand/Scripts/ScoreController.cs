58d2bd2b42a7e824931a151ae2870d0b3dbda1f6
akared-coke <aka.red.coke@gmail.com>
2021-01-26 12:05:05 +0800

    Add score controller.

---
+++ Assets/MissileCommand/Scripts/ScoreController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField]
+    private TMPro.TMP_Text scoreText;
+
+    private int _currentPoints;
+    private int _highScore;
+
+    private void Start()
+    {
+        CurrentPoints = 0;
+    }
+
+    public int CurrentPoints
+    {
+        get
+        {
+            return _currentPoints;
+        }
+
+        set
+        {
+            _currentPoints = value;
+            scoreText.text = $"{_currentPoints}";
+        }
+    }
+
+    public static ScoreController Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
