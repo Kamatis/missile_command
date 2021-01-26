1573d8bdb7de1492cb441664456684b42f0b0a4a
akared-coke <aka.red.coke@gmail.com>
2021-01-26 11:09:34 +0800

    Add targeting enemy script.

---
+++ Assets/MissileCommand/Scripts/Enemies/TargetingEnemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TargetingEnemy : Enemy
+{
+    public Vector3 target = Vector3.zero;
+
+    private void Start()
+    {
+        
+    }
+
+    public override void Attack()
+    {
+        
+    }
+
+    public override void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+    }
+}
