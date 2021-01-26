9217a5bb14b3b4107fca9be4374300c388cc080d
akared-coke <aka.red.coke@gmail.com>
2021-01-26 12:40:07 +0800

    Add user.

---
+++ Assets/MissileCommand/Scripts/User.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    public int scorePoints;
+    public int highScore;
+    public int powerLevel;
+    public int reloadLevel;
+    public int speedLevel;
+    public int rebuildLevel;
+}
